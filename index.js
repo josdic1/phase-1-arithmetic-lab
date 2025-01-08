6bdedaf1bba255d363ce22061c5f5a2b3f9e6904
josdic1 <joshuadicker@gmail.com>
2025-01-08 10:57:42 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+ let num1 = 31
+ let num2 = 2
+ let num3 = 10
+ let num4 = 6
+ let num5 = 20
+ let multiply = num1*num2
+ let mod = num3 % num4
+ let max = Math.max(num2, num3, num4, num5)
 
-// Write your code here
+ let random = Math.floor(Math.random() * 100) + 1
+
