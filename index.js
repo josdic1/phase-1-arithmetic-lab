087219cbbbe1b7ae6adf34e1ac159413fe7b9776
Josh Dicker <joshdicker@joshs-mac-mini.mynetworksettings.com>
2025-03-18 13:57:39 -0400

    finished phase-1-arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31
+let num2 = 2
 
-// Write your code here
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) +1
+
+let num3 = 32
+let num4 = 8
+
+let mod = num3 / num4
+
+let max = Math.max(1,10,20)
